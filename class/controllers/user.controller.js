4f07ccea2b3642ad4de6ba14610961f91edde558
ersancakra <ersancakra@gmail.com>
2023-09-05 22:47:54 +0700

    buat method login

--- class/controllers/user.controller.js
+++ class/controllers/user.controller.js
@@ -21,7 +21,7 @@ class UserController {
                 // call service login.
                 const login = await this.userService.login(bodyJson);
 
-                res.writeHead(201, { "Content-Type": "application/json" });
+                res.writeHead(200, { "Content-Type": "application/json" });
                 res.end(JSON.stringify({ message: login, status: "success", code: 200 }, null, 2));
             } catch (error) {
                 res.writeHead(400, { "Content-Type": "application/json" });
